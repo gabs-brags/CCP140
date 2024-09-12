c3a65c702339ff03e6f4b37c2228e89d3cf541fa
gabs-brags <uniegbrag@fei.edu.br>
2024-09-12 01:37:00 +0000

    Atualização 1.3

--- Pessoa.cpp
+++ Pessoa.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Data.cpp"
+#include "Endereco.cpp"
 using namespace std;
 
 class Pessoa{
@@ -8,10 +9,11 @@ class Pessoa{
         string sobrenome;
         long cpf;
         Data nascimento;
+        Endereco endereco;
 
     public:
-        Pessoa(string nome, string sobrenome, long cpf, int dia, int mes, int ano):
-        nome{nome}, sobrenome{sobrenome}, nascimento{dia, mes, ano}
+        Pessoa(string nome, string sobrenome, long cpf, int dia, int mes, int ano, Endereco endereco):
+        nome{nome}, sobrenome{sobrenome}, nascimento{dia, mes, ano}, endereco{endereco}
         {
             this->cpf = cpf;
         }
@@ -47,5 +49,9 @@ class Pessoa{
 
         string getNascimento(){
             return this->nascimento.getData();
+        }
+
+        string getEndereco(){
+            return this->endereco.getEndereco();
         }
 };
--- main.cpp
+++ main.cpp
@@ -3,10 +3,12 @@
 using namespace std;
 
 int main(){
-    Pessoa p("Gabi", "Magalhães", 12345677890, 9, 11, 2024);
+    Endereco e("Rua R", 1, "Bairro B");
+    Pessoa p("Gabi", "Magalhães", 12345677890, 9, 11, 2024, e);
 
     cout << "Dados da Pessoa" << endl;
     cout << "Nome: " << p.getNome() << " " << p.getSobrenome() << endl;
     cout << "CPF: " << p.getCPF() << endl;
     cout << "Data de Nascimento: " << p.getNascimento() << endl;
+    cout << "Endereço: " << p.getEndereco() << endl;
 }
