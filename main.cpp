bc37693ab46a310d039d2d6ccea49491b701567e
gabs-brags <uniegbrag@fei.edu.br>
2024-09-18 21:12:28 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main(){
     Endereco e("Rua R", 1, "Bairro B");
-    Pessoa p("Gabi", "Magalhães", 12345677890, 9, 11, 2024, e);
+    Pessoa p("Nome", "Sobrenome",123456789, 9, 11, 2024, e);
 
-    cout << "Dados da Pessoa" << endl;
+    cout << "Dados da pessoa" << endl;
     cout << "Nome: " << p.getNome() << " " << p.getSobrenome() << endl;
     cout << "CPF: " << p.getCPF() << endl;
-    cout << "Data de Nascimento: " << p.getNascimento() << endl;
-    cout << "Endereço: " << p.getEndereco() << endl;
+    cout << "Data de nascimento: " << p.getNascimento() << endl;
+    cout << "Endereco: " << p.getEndereco() << endl;
 }
