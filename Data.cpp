64eb8972756b95747c52f3ce015a14890b4ee4dd
gabs-brags <uniegbrag@fei.edu.br>
2024-09-18 21:10:27 -0300

    Update Data.cpp

--- Data.cpp
+++ Data.cpp
@@ -4,12 +4,11 @@ using namespace std;
 class Data{
     private:
         int dia, mes, ano;
-
+    
     public:
-        Data(int dia, int mes, int ano):
-        dia{dia}, mes{mes}, ano{ano}{}
+        Data(int dia, int mes, int ano): dia{dia}, mes{mes}, ano{ano}{}
 
         string getData(){
-            return to_string(dia) + "/" + to_string(mes) + "/" + to_string(ano);
+            return to_string(dia)+"/"+to_string(mes)+"/"+to_string(ano);
         }
 };
